535cb3e551b73bb08ae10b3e0c38fb04d45015e6
Brigitte Huneke <brigittehuneke@gmail.com>
2017-11-22 13:56:23 -0600

    DOM-Practice

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,51 +1,59 @@
 window.onload = function() {
-  let listAlert = setListMessage();
-  alert(listAlert);
+  //TASK 1:  Alert the user to how many list items there are on the page.
+  let listCountMessage = setListMessage();
+  alert(listCountMessage);
 
-  createListHeading();
+  //TASK 2:  Add a <h2> to the page under the title that says 'You have _ items in your shopping cart'. Do not use html to do this. Use JavaScript.
+  createListCountHeaderElement();
+  updateListCountHeaderMessage();
 
-  listItemsHeading();
+  //Part of Task 4
+  addRemoveButtonsToExistingItems();
 
-  addRemoveButtonsToExisting();
+  //Used to implement task 3
+  createNewItemTextInput();
 
-  createAddItemInput();
-};
-
-function setListMessage() {
-  let list = document.getElementById("shopping-list");
-  return (
-    "This page has " + list.children.length + " items in the shopping cart."
-  );
 }
 
-function createListHeading(message) {
+//Used for task 1 & 2
+function setListMessage() {
+  let list = document.getElementById("shopping-list");
+  return "This page has " + list.children.length + " items in the shopping cart.";
+}
+
+//Used for task 2
+function createListCountHeaderElement(message) {
   let listHeading = document.createElement("h2");
   document.getElementsByTagName("h1")[0].appendChild(listHeading);
   listHeading.setAttribute("id", "list-heading");
 }
 
-function listItemsHeading() {
+//Used for task 2, 3, & 4
+function updateListCountHeaderMessage() {
   let listAlert = setListMessage();
   let listHeading = document.getElementById("list-heading");
   listHeading.innerHTML = listAlert;
 }
 
-function addRemoveButtonsToExisting() {
+//Used for task 4
+function addRemoveButtonsToExistingItems() {
   let list = document.getElementById("shopping-list");
   for (let i = 0; i < list.children.length; i++) {
     addRemoveButton(list.children[i]);
   }
 }
 
+//Used for task 3 & 4 ()
 function addRemoveButton(appendElement) {
   let removeButton = document.createElement("button");
   removeButton.innerHTML = "Remove";
   removeButton.onclick = deleteItem;
   appendElement.appendChild(removeButton);
-  listItemsHeading();
+  updateListCountHeaderMessage();
 }
 
-function createAddItemInput() {
+//Used for task 3
+function createNewItemTextInput() {
   let textField = document.getElementById("item-text");
   textField.setAttribute("placeholder", "type item to add");
 
@@ -57,6 +65,7 @@ function createAddItemInput() {
   document.getElementById("add-item").appendChild(addButton);
 }
 
+//TASK 3:  Create the ability to add more items to the list using JavaScript.
 function addItem(event) {
   event.preventDefault();
 
@@ -64,12 +73,14 @@ function addItem(event) {
   let newItem = document.createElement("li");
   newItem.innerHTML = newItemText;
   document.getElementById("shopping-list").appendChild(newItem);
-  listItemsHeading();
+  updateListCountHeaderMessage();
 
   addRemoveButton(newItem);
+  document.getElementById('add-item').reset();
 }
 
+//TASK 4: Create the ability to remove an item from the shopping cart using javascript.
 function deleteItem() {
   this.parentNode.remove(this);
-  listItemsHeading();
+  updateListCountHeaderMessage();
 }
